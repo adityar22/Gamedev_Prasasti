5f11c7e3be35ab1852a1661b15662a02225b225c
adityar22 <adityaramadhanarckarito47@gmail.com>
2023-12-19 18:54:08 +0700

    add feat: about scene ui

---
+++ Assets/Scripts/Setting/SettingValue.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingValue
+{
+    //Graphic Field
+    public static bool useEffect;
+    public static bool useParticle;
+
+    //Audio Field
+    public static float volume;
+    public static bool enableBGM;
+    public static bool enableSFX;
+
+    //Language Field
+    public static int indexLanguage;
+}
--- Assets/Scripts/SettingScene.cs
+++ Assets/Scripts/SettingScene.cs
@@ -8,46 +8,32 @@ public class SettingScene : MonoBehaviour
     private int indexTab = 0;
     private int IndexTab
     {
-        get{return indexTab;}
+        get { return indexTab; }
         set
-        { 
+        {
             indexTab = value;
             changeOption();
         }
     }
 
-    [SerializeField] private GameObject[] btnOption = new GameObject[]{};
+    [SerializeField] private GameObject[] btnOption = new GameObject[] { };
 
-    [SerializeField] private GameObject[] panelOption = new GameObject[]{};
+    [SerializeField] private GameObject[] panelOption = new GameObject[] { };
 
     //Graphic Field
+    [SerializeField] private Button[] effectBtn = new Button[] { };
+    [SerializeField] private Button[] particleBtn = new Button[] { };
 
     //Audio Field
     [SerializeField] private Slider volumeSlider;
-    [SerializeField] private Button[] bgmBtn = new Button[]{};
-    [SerializeField] private Button[] sfxBtn = new Button[]{};
+    [SerializeField] private Button[] bgmBtn = new Button[] { };
+    [SerializeField] private Button[] sfxBtn = new Button[] { };
 
     //Language Field
     // Start is called before the first frame update
     void Start()
     {
-        for(int i = 0; i < btnOption.Length; i++){
-            Button theButton = btnOption[i].GetComponent<Button>();
-            ColorBlock theColor = btnOption[i].GetComponent<Button>().colors;
-            theColor.normalColor = i == indexTab ?  new Color(255, 255, 255) :  new Color(188, 188, 188);
-            theColor.highlightedColor = new Color(255, 255, 255);
-            theColor.pressedColor = new Color(188, 188, 188) ;
-            theButton.colors = theColor;
-            
-            bool isActived = i == indexTab ? true : false;
-            panelOption[i].SetActive(isActived);
-        }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        IndexTab = 0;
     }
 
     public void onChangeOption(int index)
@@ -57,16 +43,77 @@ public class SettingScene : MonoBehaviour
 
     private void changeOption()
     {
-        for(int i = 0; i < btnOption.Length; i++){
+        for (int i = 0; i < btnOption.Length; i++)
+        {
             Button theButton = btnOption[i].GetComponent<Button>();
             ColorBlock theColor = btnOption[i].GetComponent<Button>().colors;
-            theColor.normalColor = i == indexTab ?  new Color(255, 255, 255) :  new Color(188, 188, 188);
-            theColor.highlightedColor = i == indexTab ?  new Color(255, 255, 255) :  new Color(188, 188, 188);
-            theColor.pressedColor = new Color(188, 188, 188) ;
+            btnOption[i].GetComponent<Image>().color = i != indexTab ? new Color(0.7f, 0.7f, 0.7f, 1.0f) : new Color(1.0f, 1.0f, 1.0f, 1.0f);
+            theColor.normalColor = i != indexTab ? new Color(0.7f, 0.7f, 0.7f, 1.0f) : new Color(1.0f, 1.0f, 1.0f, 1.0f);
+
             theButton.colors = theColor;
-            
+
             bool isActived = i == indexTab ? true : false;
             panelOption[i].SetActive(isActived);
         }
     }
+
+    //Manage Graphic Setting
+    public void onChangeEffectSetting(bool state)
+    {
+        SettingValue.useEffect = state;
+
+        int index = 0;
+        int active = SettingValue.useEffect != true ? 0 : 1;
+        foreach (var btn in effectBtn)
+        {
+            btn.interactable = index == active ? true : false;
+            index++;
+        }
+    }
+
+    public void onChangeParticleSetting(bool state)
+    {
+        SettingValue.useParticle = state;
+
+        int index = 0;
+        int active = SettingValue.useParticle != true ? 0 : 1;
+        foreach (var btn in particleBtn)
+        {
+            btn.interactable = index == active ? true : false;
+            index++;
+        }
+    }
+
+    //Manage Audio Setting
+    public void onVolumeUpdate()
+    {
+        SettingValue.volume = volumeSlider.value;
+    }
+    public void onChangeBGMSetting(bool state)
+    {
+        SettingValue.enableBGM = state;
+
+        int index = 0;
+        int active = SettingValue.enableBGM != true ? 0 : 1;
+        foreach (var btn in bgmBtn)
+        {
+            btn.interactable = index == active ? true : false;
+            index++;
+        }
+    }
+
+    public void onChangeSFXSetting(bool state)
+    {
+        SettingValue.enableSFX = state;
+
+        int index = 0;
+        int active = SettingValue.enableSFX != true ? 0 : 1;
+        foreach (var btn in sfxBtn)
+        {
+            btn.interactable = index == active ? true : false;
+            index++;
+        }
+    }
+
+    //Manage Language Setting
 }
